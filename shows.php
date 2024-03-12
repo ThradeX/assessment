ae7439f6f25ce6c4e1fbd7ef02d6872874c1599d
Cristyan Morais <cristyan.morais123@gmail.com>
2024-03-12 10:20:17 -0300

    fala weber

--- shows.php
+++ shows.php
@@ -3,40 +3,12 @@ include('./database/connection.php');
 
 require_once './components/header.php';
 
-// function addToCart($showId) {
-//     echo "add to cart";
-//     if (!isset($_COOKIE['cart'])) {
-//         $cart = array();
-//     } else {
-//         $cart = json_decode($_COOKIE['cart'], true);
-//     }
-//     $cart[] = $showId;
-//     setcookie('cart', json_encode($cart), time() + (86400), "/");
-// }
-
-$results_per_page = 5;
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-} else {
-    $page = 1;
-}
-
-$start_index = ($page - 1) * $results_per_page;
-
 $search = isset($_GET['search']) ? $mysqli->real_escape_string($_GET['search']) : '';
-
-$order = isset($_GET['order']) ? $mysqli->real_escape_string($_GET['order']) : 'name';
 
 $filter = isset($_GET['others']) ? $mysqli->real_escape_string($_GET['others']) : 'all';
 
-$sql = "SELECT * FROM shows WHERE name_show LIKE '%$search'";
+$sql = "SELECT * FROM shows WHERE name_show LIKE '%$search%'";
 
-$res = $mysqli->query($sql);
-$qtdShows = $res->num_rows;
-
-$sql = "SELECT * FROM shows WHERE name_show LIKE '%$search'";
-
-$sql .= " LIMIT $start_index, $results_per_page";
 $res = $mysqli->query($sql);
 $qtd = $res->num_rows;
 ?>
@@ -66,7 +38,7 @@ $qtd = $res->num_rows;
 <section id="courses">
     <div class="container">
         <div class="filter">
-        <form class="form-filter" action="" method="GET">
+            <form class="form-filter" action="" method="GET">
                 <div class="search">
                     <button type="submit">
                         <img src="./icons/search.svg" alt="search">
@@ -83,54 +55,33 @@ $qtd = $res->num_rows;
                         if ($qtd > 0) {
                             while ($row = $res->fetch_object()) {
                                 $available = $row->max_tickets - $row->bought;
-                                echo '
-                                <li id="show_<?php echo $row->id_show; ?>" class="card">
-                                <div class="overlay" onclick="addToCart(' . $row->id_show .')"></div>
+                                echo ' <li id="show_<?php echo $row->id_show; ?>" class="card">
+                                        <div class="overlay" onclick="addToCart(' . $row->id_show .')"></div>
                                         <div class="card-image">
                                             <img src="' . $row->image_show . '" alt="show cover">
                                         </div>
                                         <div class="card-title">
                                             <h4 class="title">' . $row->name_show . '</h4>
-                                            <h4 class="artist">' . $row->name_artist . '</p>                                                                                           
+                                            <h4 class="artist">' . $row->name_artist . '</h4>                                                                                           
                                         </div>
                                         <div class="card-description">
-                                            <p>' . $row->description_show . '</p>                                                                                           
-                                        </div>
-                                        <div class="price">
-                                            <p>' . $available . ' tickets left</p>
-                                            <h4>$' . $row->price . '</h4>
-                                        </div>
-                                        <div class="overlay-text">ADD TO CART</div>
+                                ';
+                                $description_lines = explode("\n", $row->description_show);
+                                foreach ($description_lines as $line) {
+                                    echo '<p>' . $line . '</p>';
+                                }
+                                echo '</div>
+                                    <div class="price">
+                                        <p>' . $available . ' tickets left</p>
+                                        <h4>$' . $row->price . '</h4>
+                                    </div>
+                                    <div class="overlay-text">ADD TO CART</div>
                                 </li>';
                             }
                         }
                     ?>
                 </ul>
             </div>
-        </div>
-        <div class="pagination">
-            <?php
-                $total_pages = ceil($qtdShows / $results_per_page);
-
-                if ($page > 1) {
-                    // echo '<a href="?page=1&order=' . $order . '&others=' . $filter . '"><<</a>';
-                    echo '<a href="?page=' . ($page - 1) . '&order=' . $order . '&others=' . $filter . '"><</a>';
-                }
-
-                for ($i = max(1, $page - 5); $i <= min($page + 5, $total_pages); $i++) {
-                    echo '<a href="?page=' . $i . '&order=' . $order . '&others=' . $filter . '&search=' . $search . '"';
-                    
-                    if ($i == $page) {
-                        echo ' class="active"';
-                    }
-                    echo '>' . $i . '</a>';
-                }
-
-                if ($page < $total_pages) {
-                    echo '<a href="?page=' . ($page + 1) . '&order=' . $order . '&others=' . $filter . '">></a>';
-                    // echo '<a href="?page=' . $total_pages . '&order=' . $order . '&others=' . $filter . '">>></a>';
-                }
-            ?>
         </div>
     </div>
     </section>
